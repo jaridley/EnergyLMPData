cd05abb7c85a3de35ff59f533dc46e5747ca0901
James A Ridley <jrid88@gmail.com>
2021-07-30 18:29:42 -0400

    More corrections

--- lmp_data.py
+++ lmp_data.py
@@ -3,13 +3,18 @@ from sqlalchemy import create_engine
 from settings import congifsettings
 
 connection = congifsettings()['alchemy_engine']
+db_table = congifsettings()['dbtable']
 db_conn = create_engine(connection)
 
 trade_date = '7/25/2021'
 lmp_data = pd_lmp.read_csv('https://docs.misoenergy.org/marketreports/20210725_da_exante_lmp.csv', skiprows=4)
 lmp_data.insert(3, 'Date', trade_date)
 data = lmp_data.head()
-data.to_sql(db_conn, name='da_exante_lmp', if_exists='append', index=False)
+try:
+    data.to_sql(db_conn, db_table, if_exists='append', index=False)
+    print('Data was imported')
+except Exception as db_error:
+    print(db_error)
 
 # print(lmp_data.columns.values)
 # print(lmp_data.dtypes)
