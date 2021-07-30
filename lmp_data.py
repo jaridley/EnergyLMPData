fe1a7a092fbaaa3c99b97b65a3928f2d231bfac4
James A Ridley <jrid88@gmail.com>
2021-07-30 18:23:55 -0400

    corrected slq connection

--- lmp_data.py
+++ lmp_data.py
@@ -9,7 +9,7 @@ trade_date = '7/25/2021'
 lmp_data = pd_lmp.read_csv('https://docs.misoenergy.org/marketreports/20210725_da_exante_lmp.csv', skiprows=4)
 lmp_data.insert(3, 'Date', trade_date)
 data = lmp_data.head()
-data.to_sql(conn=db_conn, name='da_exante_lmp', if_exists='append', index=False)
+data.to_sql(db_conn, name='da_exante_lmp', if_exists='append', index=False)
 
 # print(lmp_data.columns.values)
 # print(lmp_data.dtypes)
