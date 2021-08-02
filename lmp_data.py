7b7e38a32148ffbf1ceebdc72fc6d8c7ae48d0cf
James A Ridley <jrid88@gmail.com>
2021-08-02 18:43:35 -0400

    fixing dataframe so that data can be inserted into the database

--- lmp_data.py
+++ lmp_data.py
@@ -9,7 +9,9 @@ db_conn = create_engine(connection)
 trade_date = '7/25/2021'
 lmp_data = pd_lmp.read_csv('https://docs.misoenergy.org/marketreports/20210725_da_exante_lmp.csv', skiprows=4)
 lmp_data.insert(3, 'Date', trade_date)
-data = lmp_data.columns.str.lower().head()
+lmp_data.columns = lmp_data.columns.str.lower()
+data = lmp_data.head()
+
 try:
     data.to_sql(db_table, db_conn, if_exists='append', index=False)
     print('Data was imported')
@@ -18,5 +20,5 @@ except Exception as db_error:
 
 # print(lmp_data.columns.values)
 # print(lmp_data.dtypes)
-# print(lmp_data.head())
+# print(data)
 
